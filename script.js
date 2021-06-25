676635489f055b638b0cfa8df9740d12e29d7b51
Zack Anderson <zack.anderson2011@gmail.com>
2021-06-24 20:50:21 -0500

    WIP added API fetch data

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-fetch('https://utelly-tv-shows-and-movies-availability-v1.p.mashape.com/lookup')
-  .then(response => response.json())
-  .then(data => console.log(data));
+fetch('http://www.omdbapi.com/?i=tt3896198&apikey=8e4cfd66', {
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
+fetch("https://api.giphy.com/v1/gifs/search?api_key=tBkIHZ6GFiU67GbmkzNnGvHO0aiWvJJg", {
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });

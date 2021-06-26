15b0aa378c8b2867a8f79a7d4108329c5e71cf87
Zack Anderson <zack.anderson2011@gmail.com>
2021-06-26 13:44:04 -0500

    added functions to script to pull from checkboxes

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ fetch('http://www.omdbapi.com/?i=tt3896198&apikey=8e4cfd66', {
     console.log(data);
   });
 
-fetch("https://api.giphy.com/v1/gifs/search?api_key=tBkIHZ6GFiU67GbmkzNnGvHO0aiWvJJg", {
+fetch("https://api.giphy.com/v1/gifs/search?api_key=tBkIHZ6GFiU67GbmkzNnGvHO0aiWvJJg&q=golf", {
 })
   .then(function (response) {
     return response.json();
@@ -15,3 +15,20 @@ fetch("https://api.giphy.com/v1/gifs/search?api_key=tBkIHZ6GFiU67GbmkzNnGvHO0aiW
   .then(function (data) {
     console.log(data);
   });
+
+  document.getElementById("search-btn").addEventListener("click",function(){
+    const ratings =[]
+  $('input.rating[type=checkbox]').each(function(){
+    if ($(this).is(":checked")){
+      ratings.push($(this).val())
+  }
+  })
+console.log(ratings)
+  })
+  //need return ratings...
+
+
+
+
+  //Create separate function for Genre and Release Year
+  //getRatings, getGenre, getRelease, getActor

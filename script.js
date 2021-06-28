838b6b792ae1689b44b9585f26d822e9a1b56002
Zack Anderson <zack.anderson2011@gmail.com>
2021-06-28 15:30:18 -0500

    Adjusted formatting of HTML and parameters for API

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-fetch('http://www.omdbapi.com/?i=tt3896198&apikey=8e4cfd66', {
-})
+let url =
+  "https://api.watchmode.com/v1/list-titles/?apiKey=yhpNItDXAOmWWzncuiKhQUljt22PagqsZ3ryDlLs&source_ids=203,57&genres=1";
+fetch(url, { method: "Get" })
+  .then((res) => res.json())
+  .then((json) => {
+    console.log(json);
+  });
+
+let url2 =
+  "https://api.watchmode.com/v1/genres/?apiKey=yhpNItDXAOmWWzncuiKhQUljt22PagqsZ3ryDlLs";
+fetch(url2, { method: "Get" })
+  .then((res) => res.json())
+  .then((json) => {
+    console.log(json);
+  });
+
+
+
+
+
+fetch("http://www.omdbapi.com/?i=tt3896198&apikey=8e4cfd66", {})
   .then(function (response) {
     return response.json();
   })
@@ -7,8 +26,10 @@ fetch('http://www.omdbapi.com/?i=tt3896198&apikey=8e4cfd66', {
     console.log(data);
   });
 
-fetch("https://api.giphy.com/v1/gifs/search?api_key=tBkIHZ6GFiU67GbmkzNnGvHO0aiWvJJg&q=golf", {
-})
+fetch(
+  "https://api.giphy.com/v1/gifs/search?api_key=tBkIHZ6GFiU67GbmkzNnGvHO0aiWvJJg&q=golf",
+  {}
+)
   .then(function (response) {
     return response.json();
   })
@@ -16,38 +37,54 @@ fetch("https://api.giphy.com/v1/gifs/search?api_key=tBkIHZ6GFiU67GbmkzNnGvHO0aiW
     console.log(data);
   });
 
-  document.getElementById("search-btn").addEventListener("click",function(){
-    const ratings =[]
-  $('input.rating[type=checkbox]').each(function(){
-    if ($(this).is(":checked")){
-      ratings.push($(this).val())
-  }
-  })
-console.log(ratings)
-  })
-  //need return ratings...
+let url =
+  "https://api.watchmode.com/v1/genres/?apiKey=yhpNItDXAOmWWzncuiKhQUljt22PagqsZ3ryDlLs";
 
+fetch(url, { method: "Get" })
+  .then((res) => res.json())
+  .then((json) => {
+    console.log(json);
+  });
 
-  document.getElementById("search-btn").addEventListener("click",function(){
-    const genre =[]
-  $('input.genre[type=checkbox]').each(function(){
-    if ($(this).is(":checked")){
-      genre.push($(this).val())
-  }
-  })
-console.log(genre)
-  })
+document.getElementById("search-btn").addEventListener("click", function () {
+  const ratings = [];
+  $("input.rating[type=checkbox]").each(function () {
+    if ($(this).is(":checked")) {
+      ratings.push($(this).val());
+    }
+  });
+  console.log(ratings);
+});
+//need return ratings...
 
-    document.getElementById("search-btn").addEventListener("click",function(){
-    const release =[]
-  $('input.release[type=checkbox]').each(function(){
-    if ($(this).is(":checked")){
-      release.push($(this).val())
-  }
-  })
-console.log(release)
-  })
+document.getElementById("search-btn").addEventListener("click", function () {
+  const genre = [];
+  $("input.genre[type=checkbox]").each(function () {
+    if ($(this).is(":checked")) {
+      genre.push($(this).val());
+    }
+  });
+  console.log(genre);
+});
 
+document.getElementById("search-btn").addEventListener("click", function () {
+  const release = [];
+  $("input.release[type=checkbox]").each(function () {
+    if ($(this).is(":checked")) {
+      release.push($(this).val());
+    }
+  });
+  console.log(release);
+});
 
-  //Create separate function for Genre and Release Year
-  //getRatings, getGenre, getRelease, getActor
+document.getElementById("search-btn").addEventListener("click", function () {
+  const actor = "";
+  $("input.actor").each(function () {
+    actor.push($(this).value());
+  });
+  console.log(actor);
+});
+
+//Create separate function for Genre and Release Year
+//getRatings, getGenre, getRelease, getActor
+

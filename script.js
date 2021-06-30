866d2d99b1af8eefe83e109106afb3c26783d9a4
Zack Anderson <zack.anderson2011@gmail.com>
2021-06-30 15:17:51 -0500

    Successfully combined objects and created hide/show functionality

--- script.js
+++ script.js
@@ -59,49 +59,61 @@ $.ajax({
 });
 */
 
-
 document.getElementById("search-btn").addEventListener("click", function () {
+  $("#response-container").show();
   const genres = getGenres();
-  const url =
-    "https://api.watchmode.com/v1/list-titles/?apiKey=yhpNItDXAOmWWzncuiKhQUljt22PagqsZ3ryDlLs&genres=" + genres;
-
-$.get(url).done(function (movies) {
-  console.log(movies);
-  for (let i = 0; i < 4; i++) {
-    $("#descr-" + i).text(descr.plot_overview);
-    $("#genre-" + i).text(genre[0].name);
-    $("#card-type-" + i).text(titles[0].titles[i].tmdb_type);
-    $("#card-year-" + i).text(titles[0].titles[i].year);
-    $("#card-title-" + i).text(titles[0].titles[i].title);
+  const types = getTypes();
+  genreValues = [];
+  for (let i = 0; i < genres.length; i++) {
+    genreValues.push(genres[i].value);
   }
-});
+  const url =
+    "https://api.watchmode.com/v1/list-titles/?apiKey=yhpNItDXAOmWWzncuiKhQUljt22PagqsZ3ryDlLs&genres=" +
+    genreValues +
+    "&types=" +
+    types;
+    console.log(url)
+
+  $.get(url).done(function (movies) {
+    console.log(movies);
+    for (let i = 0; i < 4; i++) {
+      let randomNumber = Math.floor(Math.random() * movies.titles.length);
+      $("#descr-" + i).text(movies.titles[randomNumber].plot_overview);
+      $("#card-type-" + i).text(movies.titles[randomNumber].tmdb_type);
+      $("#card-year-" + i).text(movies.titles[randomNumber].year);
+      $("#card-title-" + i).text(movies.titles[randomNumber].title);
+    }
+  });
 });
 
 function getGenres() {
   const genre = [];
   $("input.genre[type=checkbox]").each(function () {
     if ($(this).is(":checked")) {
-      genre.push($(this).val());
+      let genreToAdd = {
+        value: $(this).val(),
+        name: $(this).attr("name"),
+      };
+      genre.push(genreToAdd);
     }
   });
   console.log(genre);
-  return genre
+  return genre;
 }
 
+function getTypes() {
+  const type = [];
+  $("input.type[type=checkbox]").each(function () {
+    if ($(this).is(":checked")) {
+      type.push($(this).val());
+    }
+  });
+  console.log(type);
+  return type;
+}
 
-//Need randomizer...
 
 /*
-genre = array
-genre.length
-let randomNumber = Math.floor(Math.random() * genre.length)
-const chosenMovies = []
-chosenMovies.push(randomNumber)
-while(chosenMovies.includes(randomNumber)) {
-randomNumber = Math.floor …
-}
-
-
 
 fetch(
   "https://api.giphy.com/v1/gifs/search?api_key=tBkIHZ6GFiU67GbmkzNnGvHO0aiWvJJg&q=golf",
@@ -135,24 +147,7 @@ document.getElementById("search-btn").addEventListener("click", function () {
 //need return ratings...
 
 
-document.getElementById("search-btn").addEventListener("click", function () {
-  const release = [];
-  $("input.release[type=checkbox]").each(function () {
-    if ($(this).is(":checked")) {
-      release.push($(this).val());
-    }
-  });
-  console.log(release);
-});
 
-document.getElementById("search-btn").addEventListener("click", function () {
-  const actor = "";
-  $("input.actor").each(function () {
-    actor.push($(this).value());
-  });
-  console.log(actor);
-});
 
-//Create separate function for Genre and Release Year
 //getRatings, getGenre, getRelease, getActor
 */

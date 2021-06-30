37f824d03f7d7c889455dd2278bc0be25343e7b6
Zack Anderson <zack.anderson2011@gmail.com>
2021-06-30 15:25:27 -0500

    cleaned up js files and added class to p tags

--- script.js
+++ script.js
@@ -7,57 +7,6 @@ let cardType = $("#card-type");
 let cardYear = $("#card-year");
 let cardDescr = $("#card-descr");
 
-/*
-fetch(url, { method: "Get" })
-  .then((res) => res.json())
-  .then((json) => {
-    console.log(json);
-  });
-
-let url2 =
-  "https://api.watchmode.com/v1/genres/?apiKey=yhpNItDXAOmWWzncuiKhQUljt22PagqsZ3ryDlLs";
-/*fetch(url2, { method: "Get" })
-  .then((res) => res.json())
-  .then((json) => {
-    console.log(json);
-  });
-*/
-
-/*
-let url =
-  "https://api.watchmode.com/v1/list-titles/?apiKey=yhpNItDXAOmWWzncuiKhQUljt22PagqsZ3ryDlLs&source_ids=203,57&genres=";
-$.ajax({
-  method: "GET",
-  url: url,
-}).then(function (titles) {
-  $("#card-type").text(titles.titles[0].tmdb_type);
-  $("#card-year").text(titles.titles[0].year);
-  $("#card-title").text(titles.titles[0].title);
-  console.log(titles);
-});
-
-let url2 =
-  "https://api.watchmode.com/v1/genres/?apiKey=yhpNItDXAOmWWzncuiKhQUljt22PagqsZ3ryDlLs";
-$.ajax({
-  method: "GET",
-  url: url2,
-}).then(function (genre) {
-  $("#genre").text(genre[0].name);
-
-  console.log(genre);
-});
-
-let url3 =
-  "https://api.watchmode.com/v1/title/345534/details/?apiKey=yhpNItDXAOmWWzncuiKhQUljt22PagqsZ3ryDlLs";
-$.ajax({
-  method: "GET",
-  url: url3,
-}).then(function (descr) {
-  $("#descr").text(descr.plot_overview);
-
-  console.log(descr);
-});
-*/
 
 document.getElementById("search-btn").addEventListener("click", function () {
   $("#response-container").show();
@@ -112,42 +61,3 @@ function getTypes() {
   return type;
 }
 
-
-/*
-
-fetch(
-  "https://api.giphy.com/v1/gifs/search?api_key=tBkIHZ6GFiU67GbmkzNnGvHO0aiWvJJg&q=golf",
-  {}
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
-
-let url =
-  "https://api.watchmode.com/v1/genres/?apiKey=yhpNItDXAOmWWzncuiKhQUljt22PagqsZ3ryDlLs";
-
-fetch(url, { method: "Get" })
-  .then((res) => res.json())
-  .then((json) => {
-    console.log(json);
-  });
-
-document.getElementById("search-btn").addEventListener("click", function () {
-  const ratings = [];
-  $("input.rating[type=checkbox]").each(function () {
-    if ($(this).is(":checked")) {
-      ratings.push($(this).val());
-    }
-  });
-  console.log(ratings);
-});
-//need return ratings...
-
-
-
-
-//getRatings, getGenre, getRelease, getActor
-*/

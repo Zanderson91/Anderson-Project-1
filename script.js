8454c76f78e6d3b4d8c2b41845f68f2d453e15c1
Zack Anderson <zack.anderson2011@gmail.com>
2021-07-10 15:04:46 -0500

    Small edits to math.floor and randomizer

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ document.getElementById("search-btn-2").addEventListener("click", function () {
   //IDs for Cards and randomizer for
   $.get(url).done(function (movies) {
     console.log(movies);
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 10; i++) {
       let randomNumber = Math.floor(Math.random() * movies.titles.length);
       $("#descr-" + i).text(movies.titles[randomNumber].plot_overview);
       $("#card-type-" + i).text(movies.titles[randomNumber].tmdb_type);

c5f9469b19ca51f4fe6cceaebdd3252438cb0526
Zack Anderson <zack.anderson2011@gmail.com>
2021-07-11 17:13:05 -0500

    New functionality to include descriptions for each and new card sizing

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ document.getElementById("search-btn-1").addEventListener("click", function () {
     console.log(movies);
     for (let i = 0; i < 4; i++) {
       let randomNumber = Math.floor(Math.random() * movies.titles.length);
-      $("#descr-" + i).text(movies.titles[randomNumber].plot_overview);
+      let details = getDetails(movies.titles[randomNumber].id).then(details=>$("#descr-" + i).text(details));
       $("#card-type-" + i).text(movies.titles[randomNumber].tmdb_type);
       $("#card-year-" + i).text(movies.titles[randomNumber].year);
       $("#card-title-" + i).text(movies.titles[randomNumber].title);
@@ -35,11 +35,28 @@ document.getElementById("search-btn-1").addEventListener("click", function () {
   });
 });
 
+function getDetails(resultID) {
+  let details = ""
+  const url0 =
+    "https://api.watchmode.com/v1/title/" +
+    resultID +
+    "/details/?apiKey=yhpNItDXAOmWWzncuiKhQUljt22PagqsZ3ryDlLs";
+  return fetch(url0, { method: "Get" })
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json.plot_overview);
+      details = json.plot_overview
+      return details
+    });
+}
+
+
 //Event Listener for Show Results
 document.getElementById("search-btn-2").addEventListener("click", function () {
   $("#response-container").show();
   const genres = getGenres();
   const types = getTypes();
+  //const details = getDetails();
   genreValues = [];
   for (let i = 0; i < genres.length; i++) {
     genreValues.push(genres[i].value);
@@ -50,12 +67,17 @@ document.getElementById("search-btn-2").addEventListener("click", function () {
     "&types=" +
     types;
   console.log(url);
+
+
+
   //IDs for Cards and randomizer for
   $.get(url).done(function (movies) {
     console.log(movies);
     for (let i = 0; i < 10; i++) {
       let randomNumber = Math.floor(Math.random() * movies.titles.length);
-      $("#descr-" + i).text(movies.titles[randomNumber].plot_overview);
+      let details = getDetails(movies.titles[randomNumber].id).then(details=>$("#descr-" + i).text(details));
+      console.log(details)
+      //$("#descr-" + i).text(details);
       $("#card-type-" + i).text(movies.titles[randomNumber].tmdb_type);
       $("#card-year-" + i).text(movies.titles[randomNumber].year);
       $("#card-title-" + i).text(movies.titles[randomNumber].title);
